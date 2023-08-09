6c86a00467e4ab4c6c44e9237fcaba465ff4db00
unknown <rashedul.alam362@gmail.com>
2023-08-09 21:08:29 +0600

    first commit

---
+++ app/product/[slug]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>product-2</div>
+  )
+}
+
+export default page
---
+++ app/product/page.js
@@ -0,0 +1,11 @@
+
+
+function page() {
+  return (
+    <div>
+        <h3>This is product page </h3>
+    </div>
+  )
+}
+
+export default page
